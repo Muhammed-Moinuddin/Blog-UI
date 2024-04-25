8144218e3ce591a035167d8f6e15cfa0af4d2b1c
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2024-04-25 13:49:59 +0500

    Worked on footer and blog grid

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faFacebookF, faTumblr, faBehance, faYoutube, faTwitter, faDribbble, faGooglePlus} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faFacebookF = faFacebookF;
+  faTumblr = faTumblr;
+  faBehance = faBehance;
+  faYoutube = faYoutube;
+  faTwitter = faTwitter;
+  faDribbble = faDribbble;
+  faGooglePlus = faGooglePlus;
 }
